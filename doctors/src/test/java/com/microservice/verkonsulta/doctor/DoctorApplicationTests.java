8f5b3e4c3c5f046eb765cb254b07d96777ba3cb4
reggie.senal <reggie.senal@sg.tribalworldwide.com>
2024-05-02 11:06:47 +0800

    initial commit

---
+++ doctors/src/test/java/com/microservice/verkonsulta/doctor/DoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.verkonsulta.doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ prescriptions/src/test/java/com/microservice/verkonsulta/prescriptions/PrescriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.verkonsulta.prescriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
