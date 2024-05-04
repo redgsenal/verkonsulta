0087a99cde3eb7d42dd81aa55885339346affe00
Reggie <reggie.senal@gmail.com>
2024-05-04 16:21:46 +0800

    updates services configs

--- patients/src/main/java/com/microservice/verkonsulta/patient/PatientApplication.java
+++ patients/src/main/java/com/microservice/verkonsulta/patient/PatientApplication.java
@@ -2,8 +2,10 @@ package com.microservice.verkonsulta.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PatientApplication {
 
 	public static void main(String[] args) {
