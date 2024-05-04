cec346b07dfa3e9450264ae911a673cdb8c91853
Reggie <reggie.senal@gmail.com>
2024-05-04 14:31:04 +0800

    add service registration and discovery eureka

---
+++ service-registration/src/test/java/com/microservices/verkonsulta/serviceregistration/ServiceRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.verkonsulta.serviceregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
